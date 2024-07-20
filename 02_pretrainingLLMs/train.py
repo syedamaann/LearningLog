646468636bc8f54982524ed2de1c2ec1b7d93031
syedamaan <sydamaannnn@gmail.com>
2024-07-20 12:46:49 +0530

    load pretraining dataset from huggingface, select text column
    sourcing datasets for pretraining: we plan to combine two different datasets for our pretraining. One, a pretraining dataset from huggingface. Two, a custom dataset that we scrape from the web (in this project, we use randomly scraped python code from Github). At this commit, we load the pretraining dataset from huggingface and in the next step we get the scraped data.

--- 02_pretrainingLLMs/train.py
+++ 02_pretrainingLLMs/train.py
@@ -8,3 +8,7 @@ from fasttext.FastText import _FastText
 
 # Suppress the warnings
 warnings.filterwarnings("ignore") 
+
+# Load the pretraining dataset and Select only the text column
+pretraining_dataset = datasets.load_dataset( "upstage/Pretraining_Dataset", split="train")
+pretraining_dataset = pretraining_dataset.select_columns(["text"]) 
