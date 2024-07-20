5be23edda62865c2ce65d56aa30cfe11b3493b4a
syedamaan <sydamaannnn@gmail.com>
2024-07-20 12:49:52 +0530

    get scraped URLs

--- 02_pretrainingLLMs/train.py
+++ 02_pretrainingLLMs/train.py
@@ -12,3 +12,16 @@ warnings.filterwarnings("ignore")
 # Load the pretraining dataset and Select only the text column
 pretraining_dataset = datasets.load_dataset( "upstage/Pretraining_Dataset", split="train")
 pretraining_dataset = pretraining_dataset.select_columns(["text"]) 
+
+# Scrape python code from Github 
+urls = [
+    "https://raw.githubusercontent.com/TheAlgorithms/Python/master/searches/double_linear_search_recursion.py",
+    "https://raw.githubusercontent.com/KosingZhu/tensorflow/master/tensorflow/python/tools/module_util.py",
+    "https://raw.githubusercontent.com/EricRemmerswaal/tensorflow/master/tensorflow/python/distribute/distribute_coordinator_context.py",
+    "https://raw.githubusercontent.com/computationalartist/tensorflow/master/tensorflow/python/ops/numpy_ops/integration_test/benchmarks/numpy_mlp.py",
+    "https://raw.githubusercontent.com/Van-an/tensorflow/master/tensorflow/python/distribute/coordinator/values.py",
+    "https://raw.githubusercontent.com/nkgwer/tensorflow/master/tensorflow/lite/tools/visualize.py",
+    "https://raw.githubusercontent.com/gitblazer/youtube-dl/master/youtube_dl/version.py",
+    "https://raw.githubusercontent.com/Joshua-Barawa/My-Photos/master/venv/lib/python3.8/site-packages/django/contrib/messages/__init__.py",
+    "https://raw.githubusercontent.com/PaliC/pytorch/master/test/fx/test_subgraph_rewriter.py"
+]
