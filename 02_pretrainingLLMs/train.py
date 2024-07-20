dc7f6107ec64875e0fcde66007b29dd5e51f66e3
syedamaan <sydamaannnn@gmail.com>
2024-07-20 12:29:45 +0530

    initial commit

---
+++ 02_pretrainingLLMs/train.py
@@ -0,0 +1,10 @@
+import warnings     
+import datasets     
+import os           
+import requests
+import heapq
+import urllib
+from fasttext.FastText import _FastText
+
+# Suppress the warnings
+warnings.filterwarnings("ignore") 
