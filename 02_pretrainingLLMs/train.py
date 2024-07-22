ceb0e41938234e345985e3b3132ce768f949363c
syedamaan <sydamaannnn@gmail.com>
2024-07-22 18:46:57 +0530

    add function for evaluation for the HF leaderboard

--- 02_pretrainingLLMs/train.py
+++ 02_pretrainingLLMs/train.py
@@ -437,3 +437,27 @@ trainer = Trainer(
 )
 # initialize the training run
 trainer.train()
+
+
+# Evaluation for the Hugging Face Leaderboard
+def h6_open_llm_leaderboard(model_name):
+  task_and_shot = [
+      ('arc_challenge', 25),
+      ('hellaswag', 10),
+      ('mmlu', 5),
+      ('truthfulqa_mc2', 0),
+      ('winogrande', 5),
+      ('gsm8k', 5)
+  ]
+
+  for task, fewshot in task_and_shot:
+    eval_cmd = f"""
+    lm_eval --model hf \
+        --model_args pretrained={model_name} \
+        --tasks {task} \
+        --device cpu \
+        --num_fewshot {fewshot}
+    """
+    os.system(eval_cmd)
+
+# h6_open_llm_leaderboard(model_name="YOUR_MODEL")
