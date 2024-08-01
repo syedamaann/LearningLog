faf3c30ce6605ab42ba74a73d9d46d27ae1ebdf0
syedamaan <sydamaannnn@gmail.com>
2024-08-01 15:34:43 +0530

    create sample dataframe and convert to pytorch tensor

--- 05_embeddingmodels/traindualencoder.py
+++ 05_embeddingmodels/traindualencoder.py
@@ -7,3 +7,14 @@ import numpy as np
 import pandas as pd
 
 from transformers import AutoTokenizer
+
+# Create a sample dataframe and convert to a pytorch tensor
+df = pd.DataFrame(
+    [
+        [4.3, 1.2, 0.05, 1.07],
+        [0.18, 3.2, 0.09, 0.05],
+        [0.85, 0.27, 2.2, 1.03],
+        [0.23, 0.57, 0.12, 5.1]
+    ]
+)
+data = torch.tensor(df.values, dtype=torch.float32)
