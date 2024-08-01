600a2960d074a17d6f5cf1b1ea2db7793ad83161
syedamaan <sydamaannnn@gmail.com>
2024-08-01 15:51:06 +0530

    define the structure of encoder model
    defining the structure of encoder model means defining the encoder class. Embedding Layer: Converts token IDs to dense vectors.
    Transformer Encoder: Processes sequences using multiple layers of transformer encoders.
    Projection Layer: Maps the encoder's output to the desired embedding dimension.

--- 05_embeddingmodels/traindualencoder.py
+++ 05_embeddingmodels/traindualencoder.py
@@ -26,3 +26,22 @@ def contrastive_loss(data):
     target = torch.arange(data.size(0))
     loss = torch.nn.CrossEntropyLoss()(data, target)
     return loss
+
+# Define the structure of encoder model
+class Encoder(torch.nn.Module):
+    def __init__(self, vocab_size, embed_dim, output_embed_dim):
+        super().__init__()
+        self.embedding_layer = torch.nn.Embedding(vocab_size, embed_dim)
+        self.encoder = torch.nn.TransformerEncoder(
+            torch.nn.TransformerEncoderLayer(embed_dim, nhead=8, batch_first=True),
+            num_layers=3,
+            norm=torch.nn.LayerNorm([embed_dim]),
+            enable_nested_tensor=False
+        )
+        self.projection = torch.nn.Linear(embed_dim, output_embed_dim)
+    
+    def forward(self, tokenizer_output):
+        x = self.embedding_layer(tokenizer_output['input_ids'])
+        x = self.encoder(x, src_key_padding_mask=tokenizer_output['attention_mask'].logical_not())
+        cls_embed = x[:,0,:]
+        return self.projection(cls_embed)
